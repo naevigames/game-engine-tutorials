1a869cbf96934cfe5724cf73f4eb4bf9e7682f71
naevidev <iamadrianiusca@gmail.com>
2024-01-24 10:08:17 +0200

    Prepared the files for tutorial 1 from chapter 2

---
+++ Code/Chapter_2_Graphics/Tutorial_1_OpenGL/main.cpp
@@ -0,0 +1,34 @@
+#include "platform_manager.hpp"
+#include "window_manager.hpp"
+
+#ifdef GLFW_PLATFORM
+#include "glfw/platform_factory.hpp"
+#endif
+
+int32_t main()
+{
+    #ifdef GLFW_PLATFORM
+    glfw::PlatformFactory platform_factory;
+    #endif
+
+    auto& platform_manager = PlatformManager::instance();
+    auto& window_manager   = WindowManager::instance();
+
+    if (!platform_manager.init(&platform_factory))
+    {
+        return -1;
+    }
+
+    window_manager.init(&platform_factory, { "chapter_2_tutorial_1", { 800, 600 } });
+
+    while (window_manager.is_active())
+    {
+        window_manager.update();
+        platform_manager.update();
+    }
+
+    window_manager.release();
+    platform_manager.release();
+
+    return 0;
+}
